f380df9c016e270ad5a4907216e163696bf0a59c
India-egotist <jborsiwala@gmail.com>
2026-05-21 18:15:45 +0530

    Server.js modified

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 
+const certificatesRouter = require('./routes/certificates');
+
 const app = express();
 const port = Number(process.env.PORT) || 3002;
 
@@ -9,9 +11,14 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// 3. Serve the CERTIS folder specifically so the PDFs can be fetched
+app.use('/CERTIS', express.static(path.join(__dirname, 'CERTIS')));
+
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.use('/certificates', certificatesRouter);
 
 app.listen(port, () => {
   console.log(`Portfolio server running at http://localhost:${port}`);
